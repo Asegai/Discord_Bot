c6aee4071b2ecef460300076fd7d3b468e2710c1
Asegai <144498317+Asegai@users.noreply.github.com>
2024-07-22 16:58:03 +0530

    Added queue
    now when you do !music or /music with another song when one is already playing, it'll be added to queue and will play next.

--- musicplayer.py
+++ musicplayer.py
@@ -9,9 +9,14 @@ import asyncio
 class MusicCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.song_queue = {}  # Dictionary to store the song queue for each guild
 
     @commands.command(name='music')
-    async def music(self, ctx, *, song_name: str):
+    async def music(self, ctx, *, song_name: str = None):
+        if song_name is None:
+            await ctx.send("Please provide a song name. Usage: !music <song_name>")
+            return
+        
         ydl_opts = {
             'format': 'bestaudio/best',
             'noplaylist': True,
@@ -37,45 +42,37 @@ class MusicCommands(commands.Cog):
             await ctx.send("You need to be in a voice channel to play music.")
             return
 
-        if ctx.voice_client:
-            if ctx.voice_client.channel != voice_channel:
-                await ctx.voice_client.disconnect()
-        else:
+        if ctx.guild.id not in self.song_queue:
+            self.song_queue[ctx.guild.id] = []
+
+        self.song_queue[ctx.guild.id].append((url, video['title']))
+
+        if not ctx.voice_client:
             try:
                 voice_client = await voice_channel.connect()
+                await self.play_next(ctx.guild)
             except Exception as e:
                 await ctx.send(f"Error connecting to voice channel: {e}")
                 return
-
-        ffmpeg_opts = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            'options': '-vn'
-        }
-
-        def after_playing(error):
-            if error:
-                print('Player error: %s' % error)
-            asyncio.run_coroutine_threadsafe(self.disconnect_after_playback(ctx.guild), self.bot.loop)
-
-        try:
-            ctx.voice_client.play(discord.FFmpegPCMAudio(url, **ffmpeg_opts), after=after_playing)
-            await ctx.send(f"Now playing: {video['title']}")
-        except Exception as e:
-            await ctx.send(f"Error playing audio: {e}")
-            await ctx.voice_client.disconnect()
+        else:
+            await ctx.send(f"Added to queue: {video['title']}")
 
     @music.error
     async def music_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please provide a song name. Usage: !music <song_name>")
         elif isinstance(error, commands.CommandInvokeError):
-            await ctx.send("There was an error processing your request.")
+            await ctx.send("There was an error processing your request, perhaps you're not connected to a VC.")
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send('You do not have the necessary permissions to use this command.') 
+            await ctx.send('You do not have the necessary permissions to use this command.')
 
     @app_commands.command(name='music', description='Play a song from YouTube')
     @app_commands.describe(song_name="Name of the song to play")
-    async def slash_music(self, interaction: discord.Interaction, song_name: str):
+    async def slash_music(self, interaction: discord.Interaction, song_name: str = None):
+        if song_name is None:
+            await interaction.response.send_message("Please provide a song name. Usage: /music <song_name>")
+            return
+
         await interaction.response.defer()
 
         ydl_opts = {
@@ -103,16 +100,26 @@ class MusicCommands(commands.Cog):
             await interaction.followup.send("You need to be in a voice channel to play music.")
             return
 
-        if interaction.guild.voice_client:
-            if interaction.guild.voice_client.channel != voice_channel:
-                await interaction.guild.voice_client.disconnect()
-        else:
+        if interaction.guild.id not in self.song_queue:
+            self.song_queue[interaction.guild.id] = []
+
+        self.song_queue[interaction.guild.id].append((url, video['title']))
+
+        if not interaction.guild.voice_client:
             try:
                 voice_client = await voice_channel.connect()
+                await self.play_next(interaction.guild)
             except Exception as e:
                 await interaction.followup.send(f"Error connecting to voice channel: {e}")
                 return
+        else:
+            await interaction.followup.send(f"Added to queue: {video['title']}")
 
+    async def play_next(self, guild):
+        if guild.id not in self.song_queue or not self.song_queue[guild.id]:
+            return
+
+        url, title = self.song_queue[guild.id].pop(0)
         ffmpeg_opts = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
             'options': '-vn'
@@ -121,23 +128,14 @@ class MusicCommands(commands.Cog):
         def after_playing(error):
             if error:
                 print('Player error: %s' % error)
-            asyncio.run_coroutine_threadsafe(self.disconnect_after_playback(interaction.guild), self.bot.loop)
+            asyncio.run_coroutine_threadsafe(self.play_next(guild), self.bot.loop)
 
         try:
-            interaction.guild.voice_client.play(discord.FFmpegPCMAudio(url, **ffmpeg_opts), after=after_playing)
-            await interaction.followup.send(f"Now playing: {video['title']}")
+            guild.voice_client.play(discord.FFmpegPCMAudio(url, **ffmpeg_opts), after=after_playing)
+            asyncio.run_coroutine_threadsafe(guild.voice_client.move_to(guild.voice_client.channel), self.bot.loop)
         except Exception as e:
-            await interaction.followup.send(f"Error playing audio: {e}")
-            await interaction.guild.voice_client.disconnect()
-
-    @slash_music.error
-    async def slash_music_error(self, interaction: discord.Interaction, error):
-        if isinstance(error, app_commands.errors.MissingRequiredArgument):
-            await interaction.followup.send("Please provide a song name. Usage: /music <song_name>")
-        elif isinstance(error, app_commands.errors.CommandInvokeError):
-            await interaction.followup.send("There was an error processing your request.")
-        elif isinstance(error, app_commands.errors.CheckFailure):
-            await interaction.followup.send('You do not have the necessary permissions to use this command.')
+            print(f"Error playing audio: {e}")
+            await guild.voice_client.disconnect()
 
     @commands.command(name='stop')
     async def stop(self, ctx):
@@ -146,6 +144,7 @@ class MusicCommands(commands.Cog):
             if voice_client.is_playing():
                 voice_client.stop()
             await voice_client.disconnect()
+            self.song_queue[ctx.guild.id] = []
             await ctx.send("Stopped playing and disconnected from the voice channel.")
         else:
             await ctx.send("I am not connected to a voice channel.")
@@ -157,6 +156,7 @@ class MusicCommands(commands.Cog):
             if voice_client.is_playing():
                 voice_client.stop()
             await voice_client.disconnect()
+            self.song_queue[interaction.guild.id] = []
             await interaction.response.send_message("Stopped playing and disconnected from the voice channel.")
         else:
             await interaction.response.send_message("I am not connected to a voice channel.")
